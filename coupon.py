09ca47a7a0660589972cf5207663fb287ab5bbc8
linukc <35877675+linukc@users.noreply.github.com>
2019-04-16 00:25:13 +0300

    rebuild

--- coupon.py
+++ coupon.py
@@ -4,27 +4,12 @@ from multiprocessing import Process, Queue
 import base64
 import io
 import vk_api
-from vk_api import VkUpload
-import requests
 from random import randint
 from os import remove
+import time
 
 
-def code_img(filename):
-    msg = b"<plain_txt_msg:img>"
-    with open(filename, "rb") as imageFile:
-        msg = msg + base64.b64encode(imageFile.read())
-    msg = msg + b"<!plain_txt_msg>"
-    return msg
 
-
-def decode_img(msg):
-    msg = msg[msg.find(b"<plain_txt_msg:img>") + len(b"<plain_txt_msg:img>"):
-              msg.find(b"<!plain_txt_msg>")]
-    msg = base64.b64decode(msg)
-    buf = io.BytesIO(msg)
-    img = Image.open(buf)
-    return img
 
 
 class Produce(object):
@@ -42,8 +27,10 @@ class Produce(object):
             qr = Image.open(self.qr())
             coupon = Image.open('template.png')
             coupon.paste(qr, (90, 100))
-            coupon.save('coupon.png')
-            self.q.put(code_img('coupon.png'))
+            path = randint(1, 999999)
+            coupon.save(f'coupon{path}.png')
+            self.q.put(f'coupon{path}.png')
+            time.sleep(1)
 
 
 class Sender(object):
@@ -52,33 +39,26 @@ class Sender(object):
 
     def vk(self):
         while True:
-            vk_session = vk_api.VkApi('***', '***')
+            #добавление в альбом
+            vk_session = vk_api.VkApi('****', '****')
             vk_session.auth(token_only=True)
             upload = vk_api.VkUpload(vk_session)
-            vk = vk_session.get_api()
-            decode_img(self.q.get()).save('post.png')
-            photo = upload.photo('post.png', album_id=262366816, group_id=181149880)
-            photos = '{}_{}'.format(photo[0]['owner_id'], photo[0]['id'])
-            url = vk.photos.getById(photos=photos, extends=0)[0]['sizes'][6]['url']
-            remove('post.png')
+            path = self.q.get()
+            photo = upload.photo(path, album_id=262366816, group_id=181149880)
+            remove(path)
+            #отправка в личные сообщения
             vk_session = vk_api.VkApi(
-                token='*****')
+                token='******')
             vk = vk_session.get_api()
-            attachments = []
-            upload = VkUpload(vk_session)
-            session = requests.Session()
-            image = session.get(url, stream=True)
-            photo = upload.photo_messages(photos=image.raw)[0]
-            attachments.append(
-                'photo{}_{}'.format(photo['owner_id'], photo['id'])
-            )
-
+            attachment_photo = 'photo'+str(photo[0].get('owner_id'))+'_'+str(photo[0].get('id'))
+            print(attachment_photo)
             vk.messages.send(
-                user_id=***,
-                attachment=','.join(attachments),
-                message='Ваш код готов!',
+                user_id=****,
+                attachment=attachment_photo,
+                message='Ваш купон готов!',
                 random_id=randint(1, 999999)
             )
+            time.sleep(2)
 
 
 if __name__ == '__main__':
@@ -86,9 +66,11 @@ if __name__ == '__main__':
     produce = Produce(q)
     sender = Sender(q)
 
-    for _ in range(2):
+    for _ in range(1):
         produce_process = Process(target=produce.coupon, args=())
         produce_process.start()
 
-    sender_process = Process(target=sender.vk, args=())
-    sender_process.start()
+    for _ in range(3):
+        sender_process = Process(target=sender.vk, args=())
+        sender_process.start()
+
